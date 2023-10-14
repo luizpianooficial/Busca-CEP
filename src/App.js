9166c601c9953736270f3d39ea755572002ec593
luizpianooficial <luizgustavo.piano18@gmail.com>
2023-10-14 19:25:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import api from './services/api';
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({});
+
+
+   async function handleSearch() {
+    if(input === ''){
+      alert("Preencha algum cep!");
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput('');
+    }catch{
+      alert("CEP não encontrado!");
+      setInput('');
+
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+
+        <input type="text" 
+        placeholder="Digite seu cep..." 
+        value={input}
+        onChange={(e) => setInput(e.target.value)}/>
+
+        <button className="buttonpesquisar" 
+        onClick={handleSearch}>
+          <FiSearch size={25} color="#fff"/>
+        </button>
+      </div>
+
+
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep} </h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+
+      </main>
+      )}
+      
+      <div className="rodape">
+        <a>Desenvolvido por <a className="link" href="https://www.linkedin.com/in/luiz-gustavo-290992276/">Luiz Gustavo</a></a>
+        </div>
+      
     </div>
+    
   );
 }
 
